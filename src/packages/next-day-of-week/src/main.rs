6d438a29984f0a72c84cbc254412a5ccc741b77e
Phil Chen <06fahchen@gmail.com>
2020-09-08 10:03:50 +0800

    feat: find the next day

--- src/packages/next-day-of-week/src/main.rs
+++ src/packages/next-day-of-week/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+
+use chrono::{Duration, NaiveDateTime};
+use eetf::{Binary, Term};
+use std::io::Cursor;
+use std::str;
+
+extern "C" {
+    pub fn hostcall_set_outputs(outputs_ptr: *const u8, outputs_len: usize);
+}
+
+#[no_mangle]
+pub fn run(params: &str) {
+    let bytes = params.as_bytes();
+    let term = Term::decode(Cursor::new(&bytes)).unwrap();
+
+    match term {
+        Term::List(ref list) => {
+            let slice = &list.elements;
+
+            match slice.first() {
+                Some(Term::Binary(date_time_value)) => next_day_of_week(date_time_value),
+                _ => (),
+            }
+        }
+        _ => (),
+    }
+}
+
+const DATE_TIME_FORMAT: &str = "%Y-%m-%dT%H:%M:%S";
+
+fn next_day_of_week(date_time_value: &Binary) {
+    let ndt_str = str::from_utf8(&date_time_value.bytes).unwrap();
+    let ndt = NaiveDateTime::parse_from_str(ndt_str, DATE_TIME_FORMAT).unwrap();
+
+    let next_day = ndt + Duration::days(7);
+    let next_day = next_day.format(DATE_TIME_FORMAT).to_string();
+    let next_day = Binary::from(next_day.into_bytes());
+
+    let term = Term::from(next_day);
+
+    let mut buf = Vec::new();
+
+    term.encode(&mut buf).unwrap();
+
+    unsafe {
+        hostcall_set_outputs(buf.as_ptr(), buf.len());
+    }
 }
