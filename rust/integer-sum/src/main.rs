7847e2bec0023c3c3b2616aa35c01e93f27bddb9
Phil Chen <06fahchen@gmail.com>
2021-01-26 18:05:11 +0800

    encode sum

--- rust/integer-sum/src/main.rs
+++ rust/integer-sum/src/main.rs
@@ -1,6 +1,6 @@
 #![no_main]
 
-use eetf::{Term, List};
+use eetf::{FixInteger, Term, List};
 use std::io::Cursor;
 use std::str;
 
@@ -16,17 +16,26 @@ pub fn run(params: &str) {
     match term {
         Term::List(ref list) => {
             let slice = &list.elements;
+
             if slice.len() == 2 {
-                let arg1 = match slice[0] {
-                    Term::FixInteger(ref fixInteger) => fixInteger.value,
+                let arg1 = match slice.first() {
+                    Some(Term::FixInteger(fix_integer)) => fix_integer.value,
+                    _ => 0,
                 };
-                let arg2 = match slice[1] {
-                    Term::FixInteger(ref fixInteger) => fixInteger.value,
+                let arg2 = match slice.last() {
+                    Some(Term::FixInteger(fix_integer)) => fix_integer.value,
+                    _ => 0,
                 };
+
                 let sum = arg1 + arg2;
+                let sum = FixInteger::from(sum);
+                let sum = Term::from(sum);
+
                 let outputs = List::from(vec![sum]);
                 let term = Term::from(outputs);
+
                 let mut buf = Vec::new();
+
                 term.encode(&mut buf).unwrap();
 
                 unsafe {
