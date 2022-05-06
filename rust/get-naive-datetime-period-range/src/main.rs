32333ac895aef02981c17e090315ae53d1c39175
Phil Chen <06fahchen@gmail.com>
2022-05-06 10:25:16 +0800

    feat: scratch get-naive-datetime-period-range

---
+++ rust/get-naive-datetime-period-range/src/main.rs
@@ -0,0 +1,18 @@
+#![cfg_attr(not(test), no_main)]
+
+use jet_programmable_rust_binding::{
+    outputs::Outputs, program, value_presenter::field_type::FieldType,
+    value_presenter::ValuePresenter,
+};
+
+fn entrypoint(inputs: Vec<ValuePresenter>) -> Outputs {
+    Outputs::build(inputs)
+}
+
+program!(entrypoint, vec![FieldType::SingleLineField]);
+
+#[cfg(test)]
+mod tests {
+    // test code goes here
+    // ...snpi
+}
