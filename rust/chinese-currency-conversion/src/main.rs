3158e10e01aae420e93cb510ca2f449f46d97a66
echojoy <echojoyse@gmail.com>
2022-11-03 13:38:55 +0800

    fix:Added rules about Chinese numbers, added tests, modified file names(Range is f64 and i64)

--- rust/chinese-currency-conversion/src/main.rs
+++ rust/chinese-currency-conversion/src/main.rs
@@ -1,8 +1,9 @@
 #![cfg_attr(not(test), no_main)]
-
+//中文大写金额数字到“元”为止的。在“元”之后，应写“整”(或“正”)字；
 pub fn int_to_chinese_string(number: i64) -> String {
-    int_number_convert(number)
+    int_number_convert(number) + "整"
 }
+//在“角”之后，可以不写“整”(或“正”)字，也可以不写；大写金额数字有“分”的，“分”后面不得写“整”(或“正”)字。
 pub fn float_to_chinese_string(number: f64) -> String {
     let mut number = number;
     let mut sum = String::new();
@@ -24,6 +25,7 @@ fn int_number_convert(number: i64) -> String {
     let mut one = number % 10;
     loop {
         //判断是否保留零比如100001那么就是十万零壹而不是有很多个零
+        //阿拉伯数字中间连续有几个“0”时：中文大写金额中间可以只写一个“零”字，如￥6007.14，应写成“人民币陆仟零柒元壹角肆分”。
         if (acc % 4 != 1 || number % 10 != 0) && (number % 10 != 0 || one != 0) {
             sum += &match_number(number % 10);
         }
@@ -32,7 +34,6 @@ fn int_number_convert(number: i64) -> String {
             break;
         }
         //这个主要是判断单位是否保留，比如壹亿零壹元，万这个单位是不需要的，我需要判断万这个范围内有没有数字不为零，如果都为零的话万这个单位需要去掉
-        //目前还在想怎么优化，但是只要有一个为true就直接执行了，对性能没有什么损失
         if acc % 4 == 0
             && (number / 10 % 10 != 0
                 || number / 100 % 10 != 0
@@ -66,6 +67,8 @@ fn float_number_convert(number: i64) -> String {
     }
     sum.chars().rev().collect()
 }
+// 中文大写金额数字应用正楷或行书填写，如:
+// 壹、贰、叁、肆、伍、陆、柒、捌、玖、拾、佰、仟、万、亿、元、角、分、零、整(正)
 fn match_number(nchar: i64) -> String {
     match nchar {
         0 => "零".to_string(),
@@ -118,20 +121,25 @@ mod tests {
 
     #[test]
     fn test_int_to_chinese_string() {
-        assert_eq!(int_to_chinese_string(0), "零元");
-        assert_eq!(int_to_chinese_string(1), "壹元");
-        assert_eq!(int_to_chinese_string(10), "壹拾元");
-        assert_eq!(int_to_chinese_string(10001), "壹万零壹元");
+        assert_eq!(int_to_chinese_string(0), "零元整");
+        assert_eq!(int_to_chinese_string(1), "壹元整");
+        assert_eq!(int_to_chinese_string(10), "壹拾元整");
+        assert_eq!(int_to_chinese_string(10001), "壹万零壹元整");
         assert_eq!(
             int_to_chinese_string(123456789),
-            "壹亿贰仟叁佰肆拾伍万陆仟柒佰捌拾玖元"
+            "壹亿贰仟叁佰肆拾伍万陆仟柒佰捌拾玖元整"
         );
         assert_eq!(
             int_to_chinese_string(123400780),
-            "壹亿贰仟叁佰肆拾万零柒佰捌拾元"
+            "壹亿贰仟叁佰肆拾万零柒佰捌拾元整"
         );
-        assert_eq!(int_to_chinese_string(100000008), "壹亿零捌元");
-        assert_eq!(int_to_chinese_string(100060008), "壹亿零陆万零捌元");
+        assert_eq!(int_to_chinese_string(100000008), "壹亿零捌元整");
+        assert_eq!(int_to_chinese_string(100060008), "壹亿零陆万零捌元整");
+        assert_eq!(
+            int_to_chinese_string(123456789987654321),
+            "壹拾贰京叁仟肆佰伍拾陆兆柒仟捌佰玖拾玖亿捌仟柒佰陆拾伍万肆仟叁佰贰拾壹元整"
+        );
+        assert_eq!(int_to_chinese_string(100000000000000001), "壹拾京零壹元整");
     }
 
     #[test]
@@ -149,6 +157,11 @@ mod tests {
         assert_eq!(
             float_to_chinese_string(123400780.5),
             "壹亿贰仟叁佰肆拾万零柒佰捌拾元伍角"
+        );
+        assert_eq!(float_to_chinese_string(6007.14), "陆仟零柒元壹角肆分");
+        assert_eq!(
+            float_to_chinese_string(56789987654321.21),
+            "伍拾陆兆柒仟捌佰玖拾玖亿捌仟柒佰陆拾伍万肆仟叁佰贰拾壹元贰角壹分"
         );
     }
 }
