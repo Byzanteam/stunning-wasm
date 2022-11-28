dcda86eeaa8cf3b253b59d95042f6bb0bb8eb1ae
joeywang <joeywdz@gmail.com>
2022-11-28 11:26:20 +0800

    feat: network test

--- rust/network/src/main.rs
+++ rust/network/src/main.rs
@@ -1,11 +1,11 @@
 #![no_main]
 use serde::{Serialize};
-
-// use jet_programmable_rust_binding::hostcalls::hostcall_set_outputs;
+use std::slice;
+use std::str;
+ //use jet_programmable_rust_binding::hostcalls::hostcall_set_outputs;
 extern "C" {
     pub fn hostcall_set_outputs(outputs_ptr: *const u8, outputs_len: usize);
-    pub fn hostcall_request_inputs(inputs_ptr: *const u8, inputs_len: usize);
-    pub fn hostcall_response_outputs(outputs_ptr: *const u8, outputs_len: usize);
+    pub fn hostcall_request_inputs(inputs_ptr: *const u8, inputs_len: usize, new_ptr: usize) -> usize;
 }
 
 #[derive(Serialize)]
@@ -16,12 +16,12 @@ struct RequestData {
     body: Option<String>,
 }
 
-// #[derive(Deserialize)]
-// struct ResponseData {
-//     status: u16,
-//     headers: Vec<(String, String)>,
-//     body: Option<String>,
-// }
+#[derive(Deserialize)]
+struct ResponseData {
+    status: u16,
+    headers: Vec<(String, String)>,
+    body: Option<String>,
+}
 #[no_mangle]
 pub fn run() {
     let json = RequestData {
@@ -30,15 +30,15 @@ pub fn run() {
         headers: Vec::new(),
         body: None,
     };
+    
     let data = serde_json::to_string(&json).unwrap();
-    unsafe {
-        hostcall_request_inputs(data.as_ptr(), data.len());
-    }
+    let mut responsedata = "";
+    responsedata = unsafe {
+        let len = hostcall_request_inputs(data.as_ptr(), data.len(), responsedata.as_ptr());
+        let slice = slice::from_raw_parts(data_string.as_ptr(), len);
+        str::from_utf8(slice).unwrap();  
+    };
+
+    let responsedata:RequestData = serde_json::from_str(responsedata).unwrap();
 }
 
-#[no_mangle]
-pub fn response(data: &str) {
-    unsafe{
-        hostcall_response_outputs(data.as_ptr(), data.len());
-    }
-}
