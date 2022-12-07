42e7ede5152541df57fa1ac6cf6b630416b63921
joeywang <joeywdz@gmail.com>
2022-12-07 16:37:03 +0800

    fixup! fix: network test

--- rust/networking-sample/src/main.rs
+++ rust/networking-sample/src/main.rs
@@ -1,16 +1,34 @@
 #![cfg_attr(not(test), no_main)]
 use jet_programmable_rust_binding::request::{network, RequestData, ResponseData};
+use jet_programmable_rust_binding::hostcalls::hostcall_set_outputs;
+#![no_main]
+use hello::{request, NetworkingRequest, NetworkingResponse, ResponseData};
+use serde_json::Value;
 
 #[no_mangle]
 pub fn run() {
-    let request_data = RequestData::new(
-        "GET".to_string(),
-        "https://httpbin.org".to_string(),
+    let request_data = NetworkingRequest::get(
+        "https://worldtimeapi.org/api/timezone/Asia/Shanghai".to_string(),
         Vec::new(),
-        None,
     );
-    let _response = {
-        let response_data: ResponseData = serde_json::from_str(network(request_data)).unwrap();
-        response_data.print_response()
+    let response = {
+        let network_response: NetworkingResponse =
+            serde_json::from_str(request(request_data)).unwrap();
+        network_response.response_data()
     };
+
+    match response {
+        ResponseData::Response(res) => {
+            if let Some(body) = res.body {
+                let body: Value = serde_json::from_str(&body).unwrap();
+                match &body["datetime"] {
+                    Value::String(ti) => unsafe { hostcall_set_outputs(ti.as_ptr(), ti.len()) },
+                    _ => panic!("Return value error, the required value was not found"),
+                };
+            } else {
+                panic!("Did not return the expected value")
+            }
+        }
+        ResponseData::Message(mes) => unsafe { hostcall_set_outputs(mes.as_ptr(), mes.len()) },
+    }
 }
