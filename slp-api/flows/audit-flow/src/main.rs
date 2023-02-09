e8731c062fa60770360b8ce4cdd73f4dcb97c7da
Joey Wang <joeywdz@gmail.com>
2023-02-09 10:21:50 +0800

    feat: add audit flow

---
+++ slp-api/flows/audit-flow/src/main.rs
@@ -0,0 +1,37 @@
+#![cfg_attr(not(test), no_main)]
+
+use flow_util::flow_util::{extract_value, pop_params, response_to_outputs};
+use jet_programmable_rust_binding::{
+    networking::{request, NetworkingRequest},
+    outputs::Outputs,
+    program,
+    value_presenter::{field_type::FieldType, ValuePresenter},
+};
+
+fn entrypoint(inputs: Vec<ValuePresenter>) -> Outputs {
+    let (base_url, headers, other_value) = pop_params(inputs).unwrap();
+    let journey_id = extract_value(other_value.get(0).unwrap(), "flow_id")
+        .unwrap_or_else(|err| panic!("{}", err.message));
+    let assignment_id = extract_value(other_value.get(1).unwrap(), "flow_id")
+        .unwrap_or_else(|err| panic!("{}", err.message));
+    let body = extract_value(other_value.get(1).unwrap(), "body")
+        .unwrap_or_else(|err| panic!("{}", err.message));
+    let url = format!("{base_url}/api/v4/yaw/journeys/{journey_id}/assignments/{assignment_id}");
+    let request_data = NetworkingRequest::put(url, headers, Some(body));
+    let response_data = request(&request_data);
+    match response_data {
+        Ok(response) => response_to_outputs(response),
+        Err(err) => panic!("error code: {}, error message: {}", err.code, err.message),
+    }
+}
+
+program!(
+    entrypoint,
+    vec![
+        FieldType::SingleLineField,
+        FieldType::SingleLineListField,
+        FieldType::NumericField,
+        FieldType::NumericField,
+        FieldType::SingleLineField,
+    ]
+);
